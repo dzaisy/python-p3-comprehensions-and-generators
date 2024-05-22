db291baf69b6c3433a353da90a705f9a5aebb89f
kudzaishe muzata <kudzaishemmuzata@gmail.com>
2024-05-22 13:36:55 +0300

    completed lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even = []
+    for num in num_list:
+        if num % 2 == 0:
+            even.append(num)
+    return even
+
 
 def make_exclamation(sentence_list):
-    pass
+    exclaimed = []
+    for sentence in sentence_list:
+        exclaimed.append(sentence + '!')
+    return exclaimed
